ffcfb18804b16a3f6aae4a8b80648258da01eadd
Mobarak19 <mobarakhossen2012@gmail.com>
2022-07-20 16:08:55 +0600

    password field added

--- Messenger/Controllers/LoginVC.swift
+++ Messenger/Controllers/LoginVC.swift
@@ -28,10 +28,27 @@ class LoginVC: UIViewController {
         field.layer.borderWidth = 1
         field.layer.borderColor = UIColor.lightGray.cgColor
         field.placeholder = "Email Address..."
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
         return field
     }()
-    
-    
+        
+    private let passwordField:UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Password..."
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        field.isSecureTextEntry = true
+        return field
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,18 +56,29 @@ class LoginVC: UIViewController {
         title = "Log In"
         view.backgroundColor = .white
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register", style: .done, target: self, action: #selector(didTappedRegister))
-        view.addSubview(logoImg)
+        view.addSubview(scrollView)
+        scrollView.addSubview(logoImg)
+        scrollView.addSubview(emailField)
+        scrollView.addSubview(passwordField)
+
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        let size = view.width/3
-        logoImg.frame = CGRect(x: (view.width - size)/2 , y: 80, width: size, height: size)
+        
+        scrollView.frame = view.bounds
+        let size = scrollView.width/3
+        
+        logoImg.frame = CGRect(x: (scrollView.width - size)/2 , y: 30, width: size, height: size)
+        
+        emailField.frame = CGRect(x: 30 , y:logoImg.bottom + 10, width: scrollView.width - 60, height: 52)
+        passwordField.frame = CGRect(x: 30 , y:emailField.bottom + 10, width: scrollView.width - 60, height: 52)
+
     }
     
     @objc func didTappedRegister(){
+        
         let vc = RegistrationVC()
-      
         vc.title = "Create Account"
         vc.modalPresentationStyle = .fullScreen
         navigationController?.pushViewController(vc, animated: true)
